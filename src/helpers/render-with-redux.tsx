d2488e030ccba8af49aaec293ef023b699d1e1eb
Евгений Новиков <evgeniy.mirea@yandex.ru>
2025-09-20 21:13:24 +0300

    test: test for remove task

--- src/helpers/render-with-redux.tsx
+++ src/helpers/render-with-redux.tsx
@@ -1,14 +1,31 @@
-import { render } from '@testing-library/react';
+import {render, type RenderOptions} from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import type {ReactElement} from "react";
+import {configureStore} from '@reduxjs/toolkit';
+import type {PropsWithChildren, ReactElement} from "react";
 import tasks from 'features/tasks/tasksSlice.ts'
+import type {RootState} from "store/store.ts";
 
-export const renderWithRedux = (ui: ReactElement, preloadedState = {}) => {
-  const store = configureStore({
-    reducer: { tasks },
+export const renderWithRedux = (
+  ui: ReactElement,
+  {
     preloadedState,
-  });
+    store = configureStore({
+      reducer: { tasks },
+      preloadedState
+    }),
+    ...renderOptions
+  }: {
+    preloadedState?: RootState,
+    store?: ReturnType<typeof configureStore>
+  } & RenderOptions = {}
+) => {
 
-  return render(<Provider store={store}>{ui}</Provider>);
+  function Wrapper({ children }: PropsWithChildren) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return {
+    store,
+    ...render(ui, { wrapper: Wrapper, ...renderOptions })
+  }
 };
